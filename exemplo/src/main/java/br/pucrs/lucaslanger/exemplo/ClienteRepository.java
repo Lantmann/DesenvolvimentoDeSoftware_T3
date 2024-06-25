8187bcd011ef6b0469e3707963181ec6fb24f7aa
Lucas Lantmann <102996915+Lantmann@users.noreply.github.com>
2024-06-25 22:44:08 +0000

    atualização

--- exemplo/src/main/java/br/pucrs/lucaslanger/exemplo/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.pucrs.lucaslanger.exemplo;
-
-import br.pucrs.lucaslanger.exemplo.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    // Interface para operações CRUD em Cliente
-}
--- exemplo/src/main/java/br/pucrs/lucaslanger/exemplo/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.pucrs.lucaslanger.exemplo;
-
-import br.pucrs.lucaslanger.exemplo.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Interface para operações CRUD em Usuario
-}
