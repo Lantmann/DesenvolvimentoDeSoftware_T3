9cb68d2a362a6dbad9ce04188c1744be2ed24d28
CoelhoGB <coelhogabriel03@gmail.com>
2024-06-27 16:38:52 -0300

    Fix import e pom

--- src/main/java/com/aplicativo/inscricoes/TfAplicativo.java
+++ src/main/java/com/aplicativo/inscricoes/TfAplicativo.java
@@ -3,17 +3,15 @@ package com.aplicativo.inscricoes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Classe principal que inicia a aplicação Spring Boot.
- */
-@SpringBootApplication  // Indica que esta é uma classe de configuração que inicia uma aplicação Spring Boot
+
+@SpringBootApplication
 public class TfAplicativo {
 
     /**
-     * Método principal que inicia a aplicação Spring Boot.
-     * @param args argumentos da linha de comando
+     * 
+     * @param args 
      */
     public static void main(String[] args) {
-        SpringApplication.run(TfAplicativo.class, args);  // Inicia a aplicação Spring Boot
+        SpringApplication.run(TfAplicativo.class, args);
     }
 }
